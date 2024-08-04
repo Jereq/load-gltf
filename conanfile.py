d0795a5d34dde4b4a48749e6772eba86e6c4b5cb
Sebastian Larsson <sjereq@gmail.com>
2024-08-04 20:17:34 +0200

    Upgrade to Conan 2
    Based on a newly created template project with Conan 2.6.0.
    
    The recipe for simdjson/2.2.2 is not compatible with Conan 2, so had to
    be updated to 2.2.3.

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,16 @@
+import os
+
 from conan import ConanFile
+from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
+from conan.tools.files import copy
+
 
 class LoadGltfConan(ConanFile):
     name = "load-gltf"
     version = "0.0.1"
+    package_type = "library"
+
     description = "A library to load and parse GLTF files."
     url = "https://github.com/Jereq/load-gltf"
     license = "MIT"
@@ -16,21 +23,27 @@ class LoadGltfConan(ConanFile):
 
     exports_sources = "CMakeLists.txt", "src/*", "include/*"
 
-    requires = "spdlog/1.10.0", "simdjson/2.2.2"
+    requires = "spdlog/1.10.0", "simdjson/2.2.3"
+
+    def validate(self):
+        check_min_cppstd(self, 20)
 
     def config_options(self):
         if self.settings.os == "Windows":
-            del self.options.fPIC
+            self.options.rm_safe("fPIC")
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
 
     def layout(self):
         cmake_layout(self)
 
     def generate(self):
-        tc = CMakeToolchain(self)
-        tc.generate()
-
         deps = CMakeDeps(self)
         deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
